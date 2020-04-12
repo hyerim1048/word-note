f4a2ebc091b5ac2d9f4e89d3230781eeae9519d7
hyerim1048 <gpflav@gmail.com>
2020-04-12 18:24:36 +0900

    word 추가 list 만들어보기

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Word Notes',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // This is the theme of your application
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Word Notes Demo'),
+      home: MyHomePage(title: 'Word Notes'),
     );
   }
 }
@@ -45,7 +37,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController customController = new TextEditingController();
-  String _word = '';
+  final List<String> _wordList = <String>[];
+
+  Widget _buildRow(String word) {
+    return ListTile(
+      title: Text(word),
+    );
+  }
+
+  Widget _buildWordList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: _wordList.length,
+        itemBuilder: (BuildContext _context, int i) {
+          return _buildRow(_wordList[i]);
+        });
+  }
 
   Future<String> _createAlertDialog(BuildContext context) {
     return showDialog(
@@ -81,41 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Your Today's Words",
-            ),
-            Text(
-              '$_word',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: _buildWordList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _createAlertDialog(context).then((onValue) {
             setState(() {
-              _word = onValue;
+              _wordList.add(onValue);
             });
           });
         },
